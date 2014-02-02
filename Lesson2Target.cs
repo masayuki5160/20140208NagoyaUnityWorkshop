8b64a67a5bf13ab43e6309fb0299bc6da8b268b2
masayuki5160 <masayuki5160@gmail.com>
2014-02-03 00:40:53 +0900

    Update Lesson2Target.cs

--- Lesson2Target.cs
+++ Lesson2Target.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 using System.Collections;
 
 public class Lesson2Target : MonoBehaviour {
-
+	// SendMessageの送り先になるObject名
 	public string TagetObjectName;
+
 	private int Point = 10;
 	private GameObject GameManager;
-	private string TargetTag = "Player";
+	// 操作キャラに付与されているタグ名
+	private string Tag = "Player";
 
 	void Start(){
 		GameManager =  GameObject.Find(TagetObjectName);
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		if(collision.gameObject.tag == TargetTag){
+		if(collision.gameObject.tag == Tag){
+			// 衝突したのがTag(プレイヤー)だったら得点獲得＆自身はきえる
 			Destroy(gameObject);
 			GameManager.SendMessage ("GetScore",  10);
 		}
