c2b5de3cd70ccb0f68a89593f23dafe45e195959
masayuki5160 <masayuki5160@gmail.com>
2014-02-08 12:57:56 +0900

    コメント追記

--- Lesson2Player.cs
+++ Lesson2Player.cs
@@ -6,6 +6,7 @@ public class Lesson2Player : MonoBehaviour {
 	public float speed;
 	
 	void Update () {
+		// キー入力がされたときの処理
 		if(Input.GetAxisRaw("Horizontal") != 0){
 			rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0);
 		}
