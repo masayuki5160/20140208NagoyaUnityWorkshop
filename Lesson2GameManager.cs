0891f553339a5cf25ceb1698cbd3df2786d1fc7c
masayuki5160 <masayuki5160@gmail.com>
2014-02-08 12:56:54 +0900

    コメントを追記

--- Lesson2GameManager.cs
+++ Lesson2GameManager.cs
@@ -2,23 +2,28 @@ using UnityEngine;
 using System.Collections;
 
 public class Lesson2GameManager : MonoBehaviour {
-
+	// ドロップ用のPrefab設定GameObject
 	public GameObject targetPrefab;
+	// ScoreのObject格納用
 	private GameObject ScoreObj;
+	// ドロップするオブジェクトの位置指定
 	private float DropPosY = 8.0f;
 	
 	void Start () {
+		// Scoreオブジェクトの取得
 		ScoreObj = GameObject.Find("Score");
 	}
 	
 	void Update () {
 		if(Time.frameCount % 100 == 0){
 			float tmpXpos = Random.Range(-8.0f, 8.0f);
+			// ドロップ用ObjectのInstantiate
 			Instantiate(targetPrefab,new Vector2(tmpXpos, DropPosY), Quaternion.identity);
 		}
 	}
 
 	void GetScore(int GetPoint){
+		// Scoreオブジェクトへのメッセージ送信処理
 		ScoreObj.SendMessage("UpdateScore",GetPoint);
 	}
 }
