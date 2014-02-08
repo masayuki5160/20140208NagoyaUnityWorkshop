2896161bf686bcb8cfb990dea84f34da38a00608
masayuki5160 <masayuki5160@gmail.com>
2014-02-08 12:59:07 +0900

    コメントの追記

--- Lesson2Score.cs
+++ Lesson2Score.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 using System.Collections;
 
 public class Lesson2Score : MonoBehaviour {
-
+	// 獲得ptの格納
 	private int nowScore = 0;
 	
-	// Update is called once per frame
 	void Update () {
+		// スコアの表示処理
 		this.guiText.text =  " SCORE:" + nowScore;
 	}
 
 	public void UpdateScore(int getScore){
+		// スコアの加算処理
 		nowScore += getScore;
 	}
 }
